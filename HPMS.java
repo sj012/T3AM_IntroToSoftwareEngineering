84ab495e6a9ed5a23c2ba679e8c2bd679265e6d7
nprinker98 <44656058+nprinker98@users.noreply.github.com>
2020-05-05 20:46:43 -0400

    Add files via upload

---
+++ HPMS.java
@@ -0,0 +1,58 @@
+/*
+ * HPMS is the client of the HPMS application for Happy Pets Patient Management System
+ */
+public class HPMS {
+	/*
+	 * isArgumentValid takes a String array as an argument and returns true only if given the following arguments in any order
+	 * --billing/--patient-portal and/or --customer-id
+	 */
+	private static int componentCode;																											
+	public static boolean isArgumentValid(String[]args) {
+		if(args[0].equals("--customer-id")&&(args[2].equals("--billing")||args[2].equals("--patient-portal"))) {
+			componentCode=1;
+			return true;
+		}
+		else if(args.length==3&&((args[0].equals("--billing")||args[0].equals("--patient-portal"))&&args[1].equals("--customer-id"))) {
+			componentCode=1;
+			return true;
+		}
+		else if(args.length==1&&(args[0].equals("--billing")||args[0].equals("--patient-portal"))) {
+			componentCode=0;
+			return true;
+		}
+		else return false;
+	}
+	public static void componentCaller(int componentCode,String[] args) {
+		if(componentCode==0) {
+			if(args[0].equals("--billing")){
+				Bill bill =new Bill();
+				System.out.println(bill.getBill());
+			}
+			else {
+				PatientPortal portal=new PatientPortal();
+				System.out.println(portal.getPatientPortal());
+				
+			}
+		}
+		else {
+			if(args[0].equals("--customer-id")&&args[2].equals("--billing")||args[0].equals("--billing")&& args[1].equals("--customer-id")){
+				//billPatient
+			}
+			else if(args[0].equals("--customer-id")&&args[2].equals("--patient-portal")||) {
+				//customerportal
+			}
+			
+			
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(!isArgumentValid(args)) {
+			System.err.println("Invalid Argument/s provided");
+			System.exit(1);
+		}
+		componentCaller(componentCode);
+
+	}
+
+}
